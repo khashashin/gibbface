61ad179e994ecf84b979b4c33f7bd4f978081d71
Yusuf Khasbulatov <khashashin@gmail.com>
2019-11-18 17:46:26 +0100

    Added whitenoise

--- gibbface/settings/production.py
+++ gibbface/settings/production.py
@@ -2,6 +2,14 @@ from .base import *
 
 DEBUG = False
 
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+COMPRESS_OFFLINE = True
+COMPRESS_CSS_FILTERS = [
+    'compressor.filters.css_default.CssAbsoluteFilter',
+    'compressor.filters.cssmin.CSSMinFilter',
+]
+COMPRESS_CSS_HASHING_METHOD = 'content'
+
 try:
     from .local import *
 except ImportError:
