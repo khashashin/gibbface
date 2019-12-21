ce0bd9e806973a00156b5e8584f7b81f764a17f2
Yusuf Khasbulatov <khashashin@gmail.com>
2019-12-21 05:00:29 +0000

    changed get env method

--- gibbface/wsgi.py
+++ gibbface/wsgi.py
@@ -11,7 +11,7 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-ENV = os.environ['ENV']
+ENV = os.getenv('ENV', '')
 
 if ENV == 'dev':
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "gibbface.settings.dev")
